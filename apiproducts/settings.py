5d43e3bf0871eea5bd294cb71f78678f21c2f98f
Celso <furuyamic@gmail.com>
2016-11-21 23:47:05 -0600

    api complete and stable

--- apiproducts/settings.py
+++ apiproducts/settings.py
@@ -123,4 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, "statics"), )
+
--- products/models.py
+++ products/models.py
@@ -7,8 +7,6 @@ class Product(models.Model):
 	mark = models.CharField(max_length=45, verbose_name='marca')
 	price = models.PositiveIntegerField(verbose_name='precio')
 	provider = models.CharField(max_length=45, verbose_name='proveedor')
-	image = models.ImageField(upload_to = 'pict_folder/', verbose_name='imagen')
+	image = models.ImageField(upload_to = 'media/', default='media/None/No-img.jpg', verbose_name='imagen')
 	def __unicode__(self):
 		return self.name
-
-
